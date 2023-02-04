2f8362f2b3c9fafaeac7409d6d4ce86065b5dc3d
maria <maria.ic.mihai@gmail.com>
2023-02-04 21:09:33 +0100

    Save state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import jwt_decode from "jwt-decode";
 
 function App() {
+  // Redux in the future
+  const [ user, setUser ] = useState({});
 
   function handleCallbackResponse(response) {
     var userObject = jwt_decode(response.credential);
-    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut (event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
   }
 
    useEffect(() => {
@@ -22,11 +30,24 @@ function App() {
          document.getElementById("signInDiv"),
          {theme: "outline", size: "large"}
      );
+
+     google.accounts.id.prompt();
    }, [])
 
   return (
     <div className="App">
       <div id="signInDiv"></div>
+
+      { Object.keys(user).length != 0 &&
+          <button onClick={ (e) => handleSignOut(e) }>Sign Out</button>
+      }
+
+      { user &&
+          <div>
+            <img src={user.picture}/>
+            <h3>{user.name}</h3>
+          </div>
+      }
     </div>
   );
 }
