8130802ce74b73f52611b53f9d2ad4dd412285c8
maria <maria.ic.mihai@gmail.com>
2023-02-04 20:33:52 +0100

    Initial login implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import jwt_decode from "jwt-decode";
 
 function App() {
+
+  function handleCallbackResponse(response) {
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+  }
+
+   useEffect(() => {
+     /* global google */
+     const google = window.google;
+
+     google.accounts.id.initialize({
+       client_id: `${process.env.REACT_APP_CLIENT_ID}`,
+       callback: handleCallbackResponse
+     })
+
+     google.accounts.id.renderButton(
+         document.getElementById("signInDiv"),
+         {theme: "outline", size: "large"}
+     );
+   }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
